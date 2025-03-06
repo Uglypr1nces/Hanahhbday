2ec08d472ef3c0fec273ffc2655c504cab6ce25f
Shpat Avdiu <uglyprincess@Shpats-MacBook-Pro.local>
2025-03-07 00:44:30 +0100

    added animations

--- website/party.js
+++ website/party.js
@@ -1,29 +1,65 @@
-const firework_speed = 10;
-const firework_width = 10
-const firework_height = 10;
-
-let firework_position_x;
-let firework_position_y;
-
-document.addEventListener('DOMContentLoaded', function() {
-    firework_position_x = 0;
-    firework_position_y = window.innerWidth / 2;
-
-    console.log(firework_position_y);
-
-    partyLoop();
-});
-
-
-function partyLoop(){
-    let firework = document.getElementById("firework-id");
-
-
-    while (firework_position_x < 50){
-        firework.style.left = firework_position_y + "px";
-        firework.style.bottom = firework_position_x + "px";
-
-        firework_position_y -= 1;
+class Firework {
+    constructor(speed, width, height, color) {
+        this.speed = speed;
+        this.width = width;
+        this.height = height;
+        this.color = color;
+        this.positionX = 0;
+        this.positionY = Math.floor(Math.random() * window.innerWidth);
+        this.element = document.createElement("div");
+        this.element.style.position = "absolute";
+        this.element.style.width = this.width + "px";
+        this.element.style.height = this.height + "px";
+        this.element.style.left = this.positionY + "px";
+        this.element.style.backgroundColor = this.color;
+        document.body.appendChild(this.element);
     }
 
+    move() {
+        if (this.positionX < screen.height / 2 + (screen.height / 4)) {
+            this.element.style.bottom = this.positionX + "px";
+            this.positionX += this.speed;
+
+            setTimeout(() => this.move(), 20);
+        } else {
+            this.explode();
+            setTimeout(() =>{
+                setTimeout(() => this.move(), 20);
+                this.positionX = 0;
+            },500); 
+        }
+    }
+
+    explode() {
+        this.element.style.width = "100px";
+        this.element.style.height = "100px";
+        this.element.style.backgroundColor = "yellow";
+
+        setTimeout(() => {
+            this.element.style.width = this.width + "px";
+            this.element.style.height = this.height + "px";
+            this.element.style.backgroundColor = this.color;
+        }, 500);
+    }
+}
+
+document.addEventListener('DOMContentLoaded', function() {
+    let fireworks = createFireworks(10);
+
+    for (let i = 0; i < fireworks.length; i++) {
+        fireworks[i].move();
+    }
+});
+
+function createFireworks(amount) {
+    const possible_colors = ["red", "green", "blue"];
+    const fireworks = [];
+
+    for (let i = 0; i < amount; i++) {
+        const random_num = Math.floor(Math.random() * possible_colors.length);
+        const firework = new Firework(10, 10, 10, possible_colors[random_num]);
+        fireworks.push(firework);
+    }
+
+    return fireworks;
 }
